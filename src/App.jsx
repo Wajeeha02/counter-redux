7c3eaa44b404e7b907e3dc6e236c1f2d5b3ec5cd
Wajeeha02 <wajeehausman978@gmail.com>
2024-07-08 11:21:18 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Counter } from './counter/Counter.jsx'
+
+function App() {
+  return (
+    <div>
+      <Counter/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/counter/Counter.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import './counter.css'
+import { decrement, increment } from './counterSlice'
+
+export function Counter() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <div className='counter'>
+        <h1>Counter using redux toolkit</h1>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  )
+}
